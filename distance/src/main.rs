7826dedca12b5230c7708c9c0c3616ae24f169a8
Elías Puma <elpumitaelias@gmail.com>
2017-04-19 23:54:57 -0500

    found out how to iterate on this

--- distance/src/main.rs
+++ distance/src/main.rs
@@ -4,7 +4,7 @@ use std::fs::File;
 use std::io::Read;
 use serde_json::{Value, Error};
 
-fn read_db(path: &str) -> Value{
+fn read_db(path: &str) -> Value {
     let mut file = File::open(path).unwrap();
     let mut data = String::new();
     file.read_to_string(&mut data).unwrap();
@@ -85,54 +85,41 @@ fn pearson_coef(x: &Vec<f32>, y: &Vec<f32>) -> f32 {
         sqr_diff_y += y[i].powf(2.0);
     }
 
-    println!("{}, {}, {}, {}",
-             prod_xy,
-             (sum_x * sum_y) / n_dims as f32,
-             sqr_diff_x,
-             sqr_diff_y);
-
     (prod_xy - (sum_x * sum_y) / n_dims as f32) /
     ((sqr_diff_x - avg_sqr_x).sqrt() * (sqr_diff_y - avg_sqr_y).sqrt())
 }
 
-fn distance(db: &Value, a: &str, b: &str, func: fn(&Vec<f32>, &Vec<f32>) -> f32) -> f32 {
-    // let vec_a: Vec<f32>;
-    // let vec_b: Vec<f32>;
-    // for cal in db["califications"] {
-    //     if cal["name"] == a {
-    //         vec_a = cal["scores"];
-    //     } 
+fn get_vector(db: &Value, name: &str) -> Vec<f32> {
+    let vec: Vec<f32> = vec![1.0];
+    let array = db["scores"][name].as_array().unwrap();
+    for elem in array.iter() {
+        println!("{} ", elem);
+    }
+    // let mut i = 0;
+    // while db["scores"][name][i] != json!(null) {
+    //     vec.push()
+    //     i++;
     // }
-    // println!("vec: {}", vec_a);
-    
-    // println!("{}", manhattan_dist(&vec_a, &vec_b));
-    2.0
+    // println!("{}", db["scores"][name]);
+
+    // match name {
+    //     "Angelica" => vec![4.75, 4.5, 5., 4.25, 4.],
+    //     _ => vec![4., 3., 5., 2., 1.],
+    // }
+    vec
+}
+
+fn distance(db: &Value, a: &str, b: &str, func: fn(&Vec<f32>, &Vec<f32>) -> f32) -> f32 {
+    let vec_a = get_vector(&db, a);
+    let vec_b = get_vector(&db, b);
+
+    func(&vec_a, &vec_b)
 }
 
 // fn prediction(k: u32, fun: fn(&Vec<f32>, &Vec<f32>) -> f32, user: str, band: str) -> f32 {}
 
 fn main() {
     let db = read_db("data/db.json");
-    println!("Distance = {}", distance(&db, "Angelica", "Bill", manhattan_dist));
-    // let users = vec!["Angelica", "Bill", "Chan", "Dan", "Harley", "Jordyn", "Sam", "Veronica"];
-    // let bands = vec!["Bues", "Brown", "Dead", "Nora", "Phoenix", "Slightly", "Strokes", "Vampire"];
-
-    // let angelica: Vec<f32> = vec![3.5, 2., 0., 4.5, 5., 1.5, 2.5, 2.];
-    // let bill: Vec<f32> = vec![2., 3.5, 4., 0., 2., 3.5, 0., 3.];
-    // let chan: Vec<f32> = vec![5., 1., 1., 3., 5., 1., 0., 0.];
-    // let dan: Vec<f32> = vec![3., 4., 4.5, 0., 3., 4.5, 4., 2.];
-    // let harley: Vec<f32> = vec![0., 4., 1., 4., 0., 0., 4., 1.];
-    // let jordyn: Vec<f32> = vec![0., 4.5, 4., 5., 5., 4.5, 4., 4.];
-    // let sam: Vec<f32> = vec![5., 2., 0., 3., 5., 4., 5., 0.];
-    // let veronica: Vec<f32> = vec![3., 0., 0., 5., 4., 2.5, 3., 0.];
-
-    // let clara: Vec<f32> = vec![4.75, 4.5, 5., 4.25, 4.];
-    // let roberts: Vec<f32> = vec![4., 3., 5., 2., 1.];
-
-    // println!("{}", manhattan_dist(&angelica, &bill));
-    // println!("{}", euclidian_dist(&angelica, &bill));
-    // println!("{}", cosine_dist(&angelica, &bill));
-    // println!("{}", pearson_coef(&clara, &roberts));
-    // println!("{}", pearson_coef(&angelica, &bill));
-    // println!("{}", prediction(3));
+    println!("Distance = {}",
+             distance(&db, "Angelica", "Bill", pearson_coef));
 }
