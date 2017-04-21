987c9dc01a5637a1aff2f0b37b79c7affe8eacc4
Elías Puma <elpumitaelias@gmail.com>
2017-04-21 15:23:06 -0500

    Added the source code for the report

--- report01/src/main.rs
+++ report01/src/main.rs
@@ -1,3 +1,20 @@
+extern crate csv;
+
+use std::fs::File;
+use csv::Reader;
+
+fn init_db(path: &str) -> Reader<File> {
+    let mut rdr = csv::Reader::from_file(path).unwrap();
+    rdr
+}
+
 fn main() {
+    let mut rdr = init_db("./data/Movie_Ratings.csv");
+
+    for record in rdr.decode() {
+        let (s1, s2, dist): (String, String, usize) = record.unwrap();
+        println!("({}, {}): {}", s1, s2, dist);
+    }
+
     println!("Hello, world!");
 }
