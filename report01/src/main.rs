75b99a17f69d79f0536cb92a3563702b5829d79f
Elías Puma <elpumitaelias@gmail.com>
2017-04-21 17:08:52 -0500

    Some code added, expecting to be able to read vectors from the files soon

--- report01/src/main.rs
+++ report01/src/main.rs
@@ -3,18 +3,24 @@ extern crate csv;
 use std::fs::File;
 use csv::Reader;
 
+mod distance;
+
+use distance::*;
+
 fn init_db(path: &str) -> Reader<File> {
     let mut rdr = csv::Reader::from_file(path).unwrap();
     rdr
 }
 
 fn main() {
-    let mut rdr = init_db("./data/Movie_Ratings.csv");
+    // let mut rdr = init_db("./data/Movie_Ratings.csv");
+    let mut rdr = init_db("./data/BX-Dump/BX-Users-2.csv");
 
     for record in rdr.decode() {
-        let (s1, s2, dist): (String, String, usize) = record.unwrap();
-        println!("({}, {}): {}", s1, s2, dist);
+        let (s1, s2, dist): (String, String, String) = record.unwrap();
+        println!("{}: {}, {}", s1, s2, dist);
     }
+
 
     println!("Hello, world!");
 }
