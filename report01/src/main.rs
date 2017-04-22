4ea7d5be144c809dcf1ca33cb9ac586c1802809d
Elías Puma <elpumitaelias@gmail.com>
2017-04-22 16:19:42 -0500

    Distance is working! :)

--- report01/src/main.rs
+++ report01/src/main.rs
@@ -34,7 +34,50 @@ struct Rating {
 
 
 
-fn distance(db: &HashMap<u32, HashMap<String, u32>>, a: u32, b: u32, func: fn(&Vec<f32>, &Vec<f32>) -> f32) -> f32 {
+fn distance(db: &mut HashMap<u32, HashMap<String, u32>>,
+            a: u32,
+            b: u32,
+            func: fn(&Vec<f32>, &Vec<f32>) -> f32)
+            -> f32 {
+    let mut vec_a = Vec::new();
+    let mut vec_b = Vec::new();
+
+    match db.get(&a) {
+        Some(mut a_ratings) => {
+            for (isbn_a, &rating_a) in a_ratings.iter() {
+                // print!("{} ", rating_a);
+                if let Some(b_ratings) = db.get(&b) {
+                    if let Some(val_b) = b_ratings.get(isbn_a) {
+                        // print!("{} ", val_b);
+                        vec_a.push(rating_a as f32);
+                        vec_b.push(rating_a as f32);
+                    } else {
+                        vec_a.push(rating_a as f32);
+                        vec_b.push(0.0);
+                    }
+                } else {
+                    panic!("b is not found!");
+                }
+            }
+        }
+        None => {}
+    }
+
+    match db.get(&b) {
+        Some(mut b_ratings) => {
+            for (isbn_b, &rating_b) in b_ratings.iter() {
+                if let Some(a_ratings) = db.get(&a) {}
+                else {
+                        vec_b.push(rating_b as f32);
+                        vec_b.push(0.0);
+                }
+            }
+        }
+        None => {}
+    }
+
+    // println!("{:?}\n{:?}", vec_a, vec_b);
+
     // let a_ratings = db.get_mut(&a);
     // let b_ratings = db.get_mut(&b);
 
@@ -74,7 +117,12 @@ fn load_book_ratings() {
     let mut rdr = init_db_reader("./data/BX-Dump/BX-Book-Ratings.csv").has_headers(false);
     let mut ratings: HashMap<u32, HashMap<String, u32>> = HashMap::new();
 
+    let mut i = 0;
     for record in rdr.decode() {
+        if i > 54000 {
+            break;
+        }
+        i += 1;
         let rating: Rating = record.unwrap();
 
         match ratings.entry(rating.user_id) {
@@ -88,6 +136,8 @@ fn load_book_ratings() {
             }
         }
     }
+
+    println!("{}", distance(&mut ratings, 11676, 278418, cosine_dist));
 
     /*********************LOAD USERS*********************/
     // let mut rdr = init_db("./data/BX-Dump/BX-Users.csv").has_headers(false);
