8c578b9d904a2ceee2239ea8755df759f9d19a0e
Elías Puma <elpumitaelias@gmail.com>
2017-04-22 16:53:46 -0500

    Prettifying some code xD

--- report01/src/main.rs
+++ report01/src/main.rs
@@ -1,13 +1,15 @@
 extern crate csv;
+extern crate test;
 extern crate rustc_serialize;
 
 mod distance;
 
 use std::fs::File;
 use csv::Reader;
-use distance::*;
 use std::collections::HashMap;
 use std::collections::hash_map::Entry::{Occupied, Vacant};
+use distance::{manhattan_dist, pearson_coef, cosine_dist};
+use test::Bencher;
 
 #[derive(RustcDecodable)]
 struct User {
@@ -32,8 +34,6 @@ struct Rating {
     rating: u32,
 }
 
-
-
 fn distance(db: &mut HashMap<u32, HashMap<String, u32>>,
             a: u32,
             b: u32,
@@ -47,10 +47,10 @@ fn distance(db: &mut HashMap<u32, HashMap<String, u32>>,
             for (isbn_a, &rating_a) in a_ratings.iter() {
                 // print!("{} ", rating_a);
                 if let Some(b_ratings) = db.get(&b) {
-                    if let Some(val_b) = b_ratings.get(isbn_a) {
+                    if let Some(rating_b) = b_ratings.get(isbn_a) {
                         // print!("{} ", val_b);
                         vec_a.push(rating_a as f32);
-                        vec_b.push(rating_a as f32);
+                        vec_b.push(*rating_b as f32);
                     } else {
                         vec_a.push(rating_a as f32);
                         vec_b.push(0.0);
@@ -66,43 +66,19 @@ fn distance(db: &mut HashMap<u32, HashMap<String, u32>>,
     match db.get(&b) {
         Some(mut b_ratings) => {
             for (isbn_b, &rating_b) in b_ratings.iter() {
-                if let Some(a_ratings) = db.get(&a) {}
-                else {
+                if let Some(a_ratings) = db.get(&a) {
+                    if let Some(_) = a_ratings.get(isbn_b) {
+                    } else {
                         vec_b.push(rating_b as f32);
-                        vec_b.push(0.0);
+                        vec_a.push(0.0);
+                    }
+                } else {
+                    panic!("a is not found!");
                 }
             }
         }
         None => {}
     }
-
-    // println!("{:?}\n{:?}", vec_a, vec_b);
-
-    // let a_ratings = db.get_mut(&a);
-    // let b_ratings = db.get_mut(&b);
-
-    // let mut vec_a = Vec::new();
-    // let mut vec_b = Vec::new();
-
-    // for (isbn_a, &rating_a) in a_ratings.iter() {
-    //     if let Some(val_b) = vec_b.get_mut(isbn_a) {
-    //         vec_a.push(rating_a as f32);
-    //         vec_b.push(rating_a as f32);
-    //     } else {
-    //         vec_a.push(rating_a as f32);
-    //         vec_b.push(0.0);
-    //     }
-    // }
-
-    // for (&isbn_b, &rating_b) in b_ratings.iter() {
-    //     match vec_a.get(isbn_b) {
-    //         Some(val_b) => {}
-    //         None => {
-    //             vec_b.push(rating_b as f32);
-    //             vec_a.push(0.0);
-    //         }
-    //     }
-    // }
 
     func(&vec_a, &vec_b)
 }
@@ -117,12 +93,12 @@ fn load_book_ratings() {
     let mut rdr = init_db_reader("./data/BX-Dump/BX-Book-Ratings.csv").has_headers(false);
     let mut ratings: HashMap<u32, HashMap<String, u32>> = HashMap::new();
 
-    let mut i = 0;
+    // let mut i = 0;
     for record in rdr.decode() {
-        if i > 54000 {
-            break;
-        }
-        i += 1;
+        // if i > 54000 {
+        //     break;
+        // }
+        // i += 1;
         let rating: Rating = record.unwrap();
 
         match ratings.entry(rating.user_id) {
@@ -136,22 +112,12 @@ fn load_book_ratings() {
             }
         }
     }
-
+    
     println!("{}", distance(&mut ratings, 11676, 278418, cosine_dist));
+    println!("{}", distance(&mut ratings, 11676, 278418, manhattan_dist));
+    println!("{}", distance(&mut ratings, 11676, 278418, pearson_coef));
 
     /*********************LOAD USERS*********************/
-    // let mut rdr = init_db("./data/BX-Dump/BX-Users.csv").has_headers(false);
-
-    // let mut i = 0;
-    // for record in rdr.decode() {
-    //     if i > 10 {
-    //         break;
-    //     }
-    //     let user: User = record.unwrap();
-    //     println!("{}: {} [{}]", user.id, user.location, user.age);
-
-    //     i += 1;
-    // }
     /****************************************************/
 }
 
