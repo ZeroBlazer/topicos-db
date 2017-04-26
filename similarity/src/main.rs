aea3812935c5c9caafde0a6adf7a1652c6f39765
Elías Puma <elpumitaelias@gmail.com>
2017-04-25 20:29:23 -0500

    Minor changes

--- similarity/src/main.rs
+++ similarity/src/main.rs
@@ -4,8 +4,11 @@ extern crate rustc_serialize;
 use std::fs::File;
 use csv::Reader;
 
-
-
 fn main() {
-    println!("Hello, world!");
+    let path = "./data/db1.csv";
+    let rdr = csv::Reader::from_file(path).unwrap();
+
+    for record in rdr.decode() {
+        
+    }
 }
