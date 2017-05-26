0d4ccd52bcc805a3332f331cbacf97c0cb0a80cc
Elías Puma <elpumitaelias@gmail.com>
2017-05-25 21:59:50 -0500

    Testing

--- projects/cross-validation/src/main.rs
+++ projects/cross-validation/src/main.rs
@@ -6,7 +6,7 @@ use database::AthlDatabase;
 fn main() {
     println!("Loading database, please wait...");
     let mut db = AthlDatabase::load("../../data/athletesTrainingSet.txt");
-    // println!("{:?}", db);
     db.standarize();
+    // println!("{:?}", db);
     println!("Database ready!\n---------------------------------------------");
 }
