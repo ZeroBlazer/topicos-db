95f652a45076cbf22dbe9d363d4ec3466245f29e
Elias Puma <elpumitaelias@gmail.com>
2017-05-27 15:44:39 -0500

    init commit for new database type

--- libs/database/src/lib.rs
+++ libs/database/src/lib.rs
@@ -126,6 +126,51 @@ impl AthlDatabase {
 }
 /*******************************************************/
 
+/******************** Athletes DB ********************/
+#[derive(Debug)]
+#[derive(RustcDecodable)]
+pub struct AthlRecord {
+    class: String,
+    height: f32,
+    weight: f32,
+}
+
+#[derive(Debug)]
+pub struct AthlDatabase {
+    keys: HashMap<String, usize>,
+    data: Vec<AthlRecord>,
+    asd_data: Vec<(f32, f32)>,
+}
+
+impl AthlDatabase {
+    pub fn from_file(path: &str) -> AthlDatabase {
+        let mut rdr = csv::Reader::from_file(path)
+            .unwrap()
+            .delimiter(b'\t')
+            .has_headers(true);
+
+        let mut keys: HashMap<String, usize> = HashMap::new();
+        let mut data: Vec<AthlRecord> = Vec::new();
+
+        let mut indx = 0;
+        for record in rdr.decode() {
+            let rcrd: (String, AthlRecord) = record.unwrap();
+            keys.insert(rcrd.0, indx);
+            data.push(rcrd.1);
+
+            indx += 1;
+        }
+
+        AthlDatabase {
+            keys: keys,
+            data: data,
+            asd_data: Vec::new()
+        }
+    }
+}
+/*******************************************************/
+
+
 
 // def test(training_filename, test_filename):
 //     """Test the classifier on a test set of data"""
