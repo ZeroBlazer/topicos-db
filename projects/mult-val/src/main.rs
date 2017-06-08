3ad5284eebdb40e8faf0679bfced77dd2a671f41
Elías Puma <elpumitaelias@gmail.com>
2017-06-08 18:36:48 -0500

    ABS_SD working in generics

--- projects/mult-val/src/main.rs
+++ projects/mult-val/src/main.rs
@@ -4,6 +4,8 @@ use database::generic::{Database, IrisRecord, MpgRecord};
 
 fn main() {
     // MpgDatabase::test("../../data/mpgTrainingSet.txt", "../../data/mpgTestSet.txt", true);
-    let db = Database::<IrisRecord>::from_file("../../data/irisTrainingSet.data");
-    // let db = Database::<MpgRecord>::from_file("../../data/mpgTrainingSet.txt");
+    let mut db = Database::<IrisRecord>::from_file("../../data/irisTrainingSet.data");
+    db.standarize();
+    let mut db = Database::<MpgRecord>::from_file("../../data/mpgTrainingSet.txt");
+    db.standarize();
 }
