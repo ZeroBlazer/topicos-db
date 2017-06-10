5911fa99740ae1360ab82d9273ce8a815aae9e63
Elías Puma <elpumitaelias@gmail.com>
2017-06-09 19:05:10 -0500

    Minor changes

--- projects/mult-val/src/main.rs
+++ projects/mult-val/src/main.rs
@@ -3,9 +3,5 @@ extern crate database;
 use database::generic::{Database, IrisRecord, MpgRecord};
 
 fn main() {
-    // let mut db = Database::<IrisRecord<String>, String>::from_file("../../data/irisTrainingSet.data");
-    // db.standarize();
-    // let mut db = Database::<MpgRecord<u32>, u32>::from_file("../../data/mpgTrainingSet.txt");
-    // db.standarize();
     Database::<MpgRecord<u32>, u32>::cross_validation("../../data/mpgTrainingSet.txt", 10, "mpgData");
 }
