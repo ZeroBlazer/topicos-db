426526003403d6fa10e338d3a70c7a58ca4b5767
Elías Puma <elpumitaelias@gmail.com>
2017-06-11 23:29:18 -0500

    Almost finished with Neural Networks

--- projects/mult-val/src/main.rs
+++ projects/mult-val/src/main.rs
@@ -1,9 +1,10 @@
 extern crate database;
 
-use database::generic::{Database, IrisRecord, MpgRecord};
+use database::generic::{Database, IrisRecord, MpgRecord, TrainingMethod};
 
 fn main() {
-    Database::<MpgRecord<u32>, u32>::cross_validation("../../data/mpgTrainingSet.txt", 10, "mpgData", false);
+    // Database::<MpgRecord<u32>, u32>::cross_validation("../../data/mpgTrainingSet.txt", 10, "mpgData", false, TrainingMethod::NearestNeighbors);
+    Database::<MpgRecord<u32>, u32>::cross_validation("../../data/mpgTrainingSet.txt", 10, "mpgData", false, TrainingMethod::NeuralNetwork);
     // Database::<IrisRecord<String>, String>::cross_validation("../../data/irisTrainingSet.data", 10, "irisData", false);
     // Database::<PirsonRecord<u32>, u32>::cross_validation("../../data/cbrilpirson.data", 10, "pirsonData");
 }
